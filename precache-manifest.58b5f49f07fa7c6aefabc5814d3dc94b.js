f28d48053206212582dc417184cddc1cfff66a25
Peter Murphy <pete@simspace.com>
2020-03-01 16:44:12 -0500

    Updates

---
+++ precache-manifest.58b5f49f07fa7c6aefabc5814d3dc94b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4f2985a7f797ee6a8d4",
+    "url": "/fp-ts-applicative-v-monad/static/js/runtime~main.d4f2985a.js"
+  },
+  {
+    "revision": "03238eca30a8c42ef5b3",
+    "url": "/fp-ts-applicative-v-monad/static/js/main.03238eca.chunk.js"
+  },
+  {
+    "revision": "d41b8b529b4790a56fba",
+    "url": "/fp-ts-applicative-v-monad/static/js/1.d41b8b52.chunk.js"
+  },
+  {
+    "revision": "03238eca30a8c42ef5b3",
+    "url": "/fp-ts-applicative-v-monad/static/css/main.092736f9.chunk.css"
+  },
+  {
+    "revision": "ac7ed533ddaa695ce17c678d69889d95",
+    "url": "/fp-ts-applicative-v-monad/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/fp-ts-applicative-v-monad/precache-manifest.58b5f49f07fa7c6aefabc5814d3dc94b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fp-ts-applicative-v-monad/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
